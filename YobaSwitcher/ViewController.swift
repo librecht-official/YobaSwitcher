5a31db2557dfae6316550da530dd61d9bbdc37ca
Vladislav Librekht <maclibrecht@gmail.com>
2023-01-01 20:31:10 +0700

    Initial Commit

---
+++ YobaSwitcher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  YobaSwitcher
+//
+//  Created by Vladislav Librecht on 01.01.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
